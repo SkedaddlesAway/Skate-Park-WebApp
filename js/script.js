102e11fe440b6c64dafbcd850a546f21420197cc
SkedaddlesAway <abraxoszz@gmail.com>
2024-12-04 23:13:58 -0600

    Style Fixes

--- js/script.js
+++ js/script.js
@@ -113,6 +113,15 @@ const menu =[
     
   ];
   
+// FUTURE IMPLEMENTATION 
+// const events = [
+//   {
+//     id: 1,
+//     category: "Little-Ones",
+
+//   }
+//  ];
+
   // get parent element
   const sectionCenter = document.querySelector(".section-center");
   const btnContainer = document.querySelector(".btn-container");
@@ -172,30 +181,3 @@ const menu =[
       });
     });
   }
-
-let app = Vue.createApp({
-    data() {
-      return {
-        activePage: 0,
-        pages: [
-          {
-            link: { text: "Home", url: "index.html" }
-          },
-          {
-            link: { text: "About", url: "about.html" }
-          },
-          {
-            link: { text: "Events", url: "events.html" }
-          },
-          {
-            link: { text: "Gallery", url: "gallery.html" }
-          },
-          {
-            link: { text: "Our Sponsors", url: "sponsors.html" }
-            
-          },
-        ],
-      };
-    },
-});
-  
